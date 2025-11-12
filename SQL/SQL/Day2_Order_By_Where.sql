944a7819dfa939c81ae8effe736382cb29b0bf19
sudharshini-codes <sudarshinisri07@gmail.com>
2025-11-12 14:59:07 +0530

    Add SQL script for record creation and queries

---
+++ SQL/SQL/Day2_Order_By_Where.sql
@@ -0,0 +1,23 @@
+ CREATE TABLE record(name TEXT,id INT,marks INT);
+INSERT INTO record VALUES('Surya',12,84);
+INSERT INTO record VALUES('ariya',23,89);
+INSERT INTO record VALUES('ayan',24,20);
+SELECT * FROM record WHERE marks>75 AND id<20;
+SELECT name FROM record ORDER BY marks DESC;
+
+
+
+Output:
+
++-------+------+-------+
+| name  | id   | marks |
++-------+------+-------+
+| Surya |   12 |    84 |
++-------+------+-------+
++-------+
+| name  |
++-------+
+| ariya |
+| Surya |
+| ayan  |
++-------+
